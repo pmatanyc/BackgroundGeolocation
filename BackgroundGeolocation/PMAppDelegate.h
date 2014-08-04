87b85bfac2c758c7d178143b32b16217b32e4b00
Paola Mata Maldonado <pmatanyc@gmail.com>
2014-08-04 11:12:15 -0400

    Initial Commit

---
+++ BackgroundGeolocation/PMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PMAppDelegate.h
+//  BackgroundGeolocation
+//
+//  Created by Paola Mata Maldonado on 8/4/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BackgroundGeolocation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BackgroundGeolocation
+//
+//  Created by Paola Mata Maldonado on 8/4/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PMAppDelegate class]));
+    }
+}
